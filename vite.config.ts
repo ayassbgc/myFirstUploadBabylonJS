139d8f3ec201cf07324d2e035912c2fafc374934
彩 <tottosan516aya@gmail.com>
2022-12-21 01:16:07 +0900

    modify base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-  base:'/myFirstUploadBabylonJS/docs/assets/index.379c9365.js/',
+  base:'/myFirstUploadBabylonJS/docs/',
   server: {
     port: 18080,
   },
