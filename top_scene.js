700808c6f9def67bf0acc40daf5194d036076c0b
彩 <tottosan516aya@gmail.com>
2022-12-21 02:48:37 +0900

    first commit

--- top_scene.js
+++ top_scene.js
@@ -3,11 +3,28 @@ import 'babylon-vrm-loader';
 
 export async function createScene(canvas, engine) {
   // シーンの作成
-  const scene = new BABYLON.Scene(engine);
+  const scene = await BABYLON.SceneLoader.LoadAsync(
+    'https://raw.githubusercontent.com/ayassbgc/MyFileUploadForBabylonjs/main/',
+    'takahashi.vrm',
+    engine
+  );
+  const vrmManager = scene.metadata.vrmManagers[0];
+
+  // Update secondary animation
+  scene.onBeforeRenderObservable.add(() => {
+    vrmManager.update(scene.getEngine().getDeltaTime());
+  });
+
+  // Model Transformation
+  vrmManager.rootMesh.translate(new BABYLON.Vector3(1, 0, 0), 1);
+
+  // Work with HumanoidBone
+  vrmManager.humanoidBone.leftUpperArm.addRotation(0, 1, 0);
+
+  // Work with BlendShape(MorphTarget)
+  vrmManager.morphing('Joy', 1.0);
 
   scene.createDefaultCameraOrLight(true, true, true);
-
-  var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
   return scene;
 }
